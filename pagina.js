87c109cc4051256998ef65a232f20b38de0026d8
dayanalima <limadayana64@gmail.com>
2023-07-05 11:49:14 -0300

    Pagina dinamica

---
+++ pagina.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',() =>{
+
+    consth1 = document.createElement('h1');
+    h1.classList.add('Titulo pagina');
+    h1.setAttribute('id','titulo');
+    h1.innerHTML = 'Pagina HTML'
+
+
+
+    document.body.appendChild(h1);
+    innerHTML = "Página dinamica"
+
+
+
+
+
+
+})
